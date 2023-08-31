9b9205894e9c798c30780c166d1657a465d132a5
root <adrianarrechea@yahoo.com.ar>
2023-08-31 14:13:53 +0000

    added allowed host 2

--- empleado/settings/prod.py
+++ empleado/settings/prod.py
@@ -11,8 +11,8 @@ from .base import *
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-ALLOWED_HOSTS = [146.190.34.238]
+DEBUG = False
+ALLOWED_HOSTS = ['146.190.34.238']
 
 
 
